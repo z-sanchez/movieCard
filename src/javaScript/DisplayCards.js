2442aac4c4244dc580ffd4dce5af964af8e73dcf
Ziek <zieksanchez3@gmail.com>
2021-09-16 02:15:19 -0500

    refactored everything but card and stars

--- src/javaScript/DisplayCards.js
+++ src/javaScript/DisplayCards.js
@@ -6,26 +6,65 @@ import Card from "./Card";
 class DisplayCards extends React.Component {
   constructor(props) {
     super(props);
+    this.setCenter = this.setCenter.bind(this);
     this.state = {
       movies: this.props.movies,
+      center: 2,
     };
   }
+
+  setCenter = (newCenter) => {
+    this.setState({ center: newCenter });
+  };
 
   renderSlider = () => {
     if (this.props.movieCount >= 4) {
       let domNode = document.querySelector(".contentFlex");
       return ReactDOM.createPortal(
-        <Slider max={this.props.movieCount} />,
+        <Slider
+          max={this.props.movieCount}
+          changeCenter={this.setCenter}
+          value={this.state.center}
+        />,
         domNode
       );
     } else return;
+  };
+
+  prepareMovies = (movies) => {
+    var newSet = movies.map((item) => {
+      return item;
+    });
+
+    if (this.props.movieCount < 4) {
+      newSet = movies;
+    } else {
+      if (this.state.center === 1) {
+        newSet[0] = movies[movies.length - 1];
+        newSet[1] = movies[0];
+        newSet[2] = movies[1];
+      } else if (this.state.center === movies.length) {
+        newSet[0] = movies[movies.length - 2];
+        newSet[1] = movies[movies.length - 1];
+        newSet[2] = movies[0];
+      } else {
+        newSet[0] = movies[this.state.center - 2];
+        newSet[1] = movies[this.state.center - 1];
+        newSet[2] = movies[this.state.center];
+      }
+    }
+
+    return newSet.slice(0, 3);
   };
 
   render() {
     let movies;
 
     if (this.props.movieCount !== 0) {
-      movies = this.props.movies;
+      movies = this.props.movies.map((item) => {
+        return item;
+      });
+      movies = this.prepareMovies(movies);
       movies = movies.map((e, index) => {
         return <Card info={e} key={index} />;
       });
@@ -42,5 +81,3 @@ class DisplayCards extends React.Component {
 }
 
 export default DisplayCards;
-
-//slider won't add at 4 movies
--- src/javaScript/Slider.js
+++ src/javaScript/Slider.js
@@ -1,15 +1,20 @@
 import React from "react";
 
 class Slider extends React.Component {
+  updateDisplay = () => {
+    this.props.changeCenter(parseInt(document.querySelector(".slider").value));
+  };
+
   render() {
     return (
       <form>
         <input
           className="slider"
-          defaultValue={0}
+          defaultValue={this.props.value}
           type="range"
           min="1"
           max={this.props.max}
+          onClick={this.updateDisplay}
         />
       </form>
     );
